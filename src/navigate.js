d67f009ffe3c9851f32a81a996b55120a3abb6ba
ABRAR AHMAD RAZA <razaahmadabrar@gmail.com>
2025-03-24 17:01:15 +0400

    captcha solve not working

--- src/navigate.js
+++ src/navigate.js
@@ -16,60 +16,70 @@ class Navigate {
 			btn = btn && btn[0];
 
 			if (!btn) {
-				try{await page.waitForNetworkIdle();
+				// try{
+				// 	await page.waitForNetworkIdle();
+				// 	await page.waitForSelector('input[type="email"]', { timeout: 3000 });
+				// 	await page.type(
+				// 		'input[type="email"]',
+				// 		"psupdates@classplus.co"
+				// 	);
+				// 	await page.click("#identifierNext");
+				// }
+				// catch(e){
+				// 	console.log("> Signed out flow");
+				// 	try{
+				// 		const signIn = await page.waitForSelector('div[data-identifier="psupdates@classplus.co"]');
+				// 		await signIn.click();
+				// 	}
+				// 	catch(e){
+				// 		console.log("> Reenter account flow");
+				// 		const btn = await page.waitForSelector('button[type="button"]');
+				// 		await btn.click();
+				// 	}
+				// }
+				// try {
+				// 	await page.waitForNetworkIdle();
+				// 	await page.waitForSelector('input[type="email"]', { timeout: 3000 });
+				// 	// await page.type(
+				// 	// 	'input[type="email"]',
+				// 	// 	"psupdates@classplus.co"
+				// 	// );
+				// 	await page.click("#identifierNext");
+				// 	try{
+				// 		await page.waitForSelector('img[alt="CAPTCHA image of text used to distinguish humans from robots"]');
+				// 		await new Promise(resolve => setTimeout(resolve, 2000));
+				// 		const imageUrl = await page.evaluate(() => {
+				// 			const img = document.querySelector('img[alt="CAPTCHA image of text used to distinguish humans from robots"]');
+				// 			return img.src;
+				// 		}
+				// 		);
+				// 		console.log("Captcha image url:", imageUrl);
+				// 		const captchaText = await solveCaptcha(imageUrl);
+				// 		const captchaInput = await page.waitForSelector('input[aria-label="Type the text you hear or see"]');
+				// 		for (let i = 0; i < captchaText.length; i++) {
+				// 			await captchaInput.type(captchaText[i], { delay: 100 });
+				// 		}
+				// 		await captchaInput.hover();
+				// 		await new Promise(resolve => setTimeout(resolve, 2000));
+				// 		const btn = await page.waitForSelector('button[type="button"]');
+				// 		await btn.click();
+				// 	}
+				// 	catch(e){
+				// 		console.log("No captcha found", e);
+				// 	}
+				// }
+				// catch(e){
+				// 	console.log("Direct password flow");
+				// }
+				// // get captcha img link id = "captchaimg"
+
+				await page.waitForNetworkIdle();
 				await page.waitForSelector('input[type="email"]', { timeout: 3000 });
 				await page.type(
 					'input[type="email"]',
 					"psupdates@classplus.co"
 				);
-				await page.click("#identifierNext");}
-				catch(e){
-					console.log("> Signed out flow");
-					try{
-						const signIn = await page.waitForSelector('div[data-identifier="psupdates@classplus.co"]');
-						await signIn.click();
-					}
-					catch(e){
-						console.log("> Reenter account flow");
-						const btn = await page.waitForSelector('button[type="button"]');
-						await btn.click();
-					}
-				}
-				try {
-					await page.waitForNetworkIdle();
-					await page.waitForSelector('input[type="email"]', { timeout: 3000 });
-					// await page.type(
-					// 	'input[type="email"]',
-					// 	"psupdates@classplus.co"
-					// );
-					await page.click("#identifierNext");
-					try{
-						await page.waitForSelector('img[alt="CAPTCHA image of text used to distinguish humans from robots"]');
-						await new Promise(resolve => setTimeout(resolve, 2000));
-						const imageUrl = await page.evaluate(() => {
-							const img = document.querySelector('img[alt="CAPTCHA image of text used to distinguish humans from robots"]');
-							return img.src;
-						}
-						);
-						console.log("Captcha image url:", imageUrl);
-						const captchaText = await solveCaptcha(imageUrl);
-						const captchaInput = await page.waitForSelector('input[aria-label="Type the text you hear or see"]');
-						for (let i = 0; i < captchaText.length; i++) {
-							await captchaInput.type(captchaText[i], { delay: 100 });
-						}
-						await captchaInput.hover();
-						await new Promise(resolve => setTimeout(resolve, 2000));
-						const btn = await page.waitForSelector('button[type="button"]');
-						await btn.click();
-					}
-					catch(e){
-						console.log("No captcha found", e);
-					}
-				}
-				catch(e){
-					console.log("Direct password flow");
-				}
-				// get captcha img link id = "captchaimg"
+				await page.click("#identifierNext");
 				console.log("Email entered > Password flow");
 				await page.waitForNavigation({ waitUntil: "networkidle0" });
 				await page.waitForSelector('input[type="password"]');
